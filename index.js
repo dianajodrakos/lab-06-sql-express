bd3a7439734d0cc35937d413072b4a12ce6a83a1
DJ Drakos <dianajodrakos@gmail.com>
2021-06-15 15:58:16 -0700

    added GET routes for single and multiple entities

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const books = require('./data.js');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send(books)
+app.use(cors());
+
+app.get('/books', (req, res) => {
+    res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    //create variable from request
+    const id = Number(req.params.id);
+
+    //find correct book by id
+    const reqBook = books.inventory.find((book) => book.id === id);
+    //return correct book
+    res.json(reqBook);
 })
 
 app.listen(port, () => {
